106c22f9b47fd309020e8214cf840f415aed532d
Deepikajogi20 <deepikajogi843@gmail.com>
2026-08-28 14:00:36 +0530

    Time: 2 ms (44.85%), Space: 19.4 MB (40.81%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -0,0 +1,14 @@
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        repeated=""
+        count=0
+        while len(repeated)<len(b):
+            repeated+=a
+            count+=1
+        if b in repeated:
+            return count
+        repeated+=a
+        count+=1
+        if b in repeated:
+            return count
+        return -1
